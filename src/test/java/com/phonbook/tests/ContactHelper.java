d1156dbdac7444906b4bdfc1eb80c0da41f98c19
Viacheslav Chekhachov <126770256+Vicheslav23@users.noreply.github.com>
2023-06-22 20:50:14 +0200

    added TestBase

---
+++ src/test/java/com/phonbook/tests/ContactHelper.java
@@ -0,0 +1,42 @@
+package com.phonbook.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class ContactHelper extends BaseHelper {
+    public ContactHelper(WebDriver driver) {
+        super(driver);
+    }
+
+    public void clickOnSaveButton() {
+        click(By.cssSelector(".add_form__2rsm2 button"));
+    }
+
+    public void fillAddContactForm(Contact contact) {
+        type(By.cssSelector("input:nth-child(1)"), contact.getName());
+        type(By.cssSelector("input:nth-child(2)"), contact.getLastname());
+        type(By.cssSelector("input:nth-child(3)"), contact.getPhone());
+        type(By.cssSelector("input:nth-child(4)"), contact.getEmail());
+        type(By.cssSelector("input:nth-child(5)"), contact.getAddress());
+        type(By.cssSelector("input:nth-child(6)"), contact.getDescription());
+    }
+    public boolean isContactCreated(String text){
+        List<WebElement> contacts = driver.findElements(By.cssSelector("h2"));
+        for (WebElement element: contacts){
+            if (element.getText().contains(text))
+                return true;
+        }
+        return false;
+    }
+    public void searchNewContact() {
+        click(By.cssSelector("h3"));
+    }
+
+    public void deleteContact() {
+        click(By.xpath("//button[text()='Remove']"));
+    }
+}
+
