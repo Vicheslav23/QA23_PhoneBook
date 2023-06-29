d95ae4a3ef1e50f2d61db77fe42adea9fa48d767
Viacheslav Chekhachov <126770256+Vicheslav23@users.noreply.github.com>
2023-06-29 10:48:58 +0200

    added testbase

--- src/test/java/com/phonebook/tests/TestBase.java
+++ src/test/java/com/phonebook/tests/TestBase.java
@@ -44,7 +44,8 @@ public class TestBase {
         if (result.isSuccess()){
             logger.info("PASSED: " + result.getMethod().getMethodName());
         }else {
-            logger.error("FAILED: " + result.getMethod().getMethodName());
+            logger.error("FAILED: " + result.getMethod().getMethodName() + " Screenshot: "
+            + app.getUser().takeScreenshot());
         }
         logger.info("Stop test");
         logger.info("--------------------------------------------------------------");
